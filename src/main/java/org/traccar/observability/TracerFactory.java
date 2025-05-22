61e979000cd59440c84f6600df21f5d9c7424e51
njdifiore91 <nick@blitzy.com>
2025-05-21 23:18:15 -0400

    File: src/main/java/org/traccar/observability/TracerFactory.java

---
+++ src/main/java/org/traccar/observability/TracerFactory.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023 - 2024 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.observability;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import org.traccar.config.Config;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+/**
+ * Factory for creating OpenTelemetry tracers.
+ */
+@Singleton
+public class TracerFactory {
+
+    private final OpenTelemetry openTelemetry;
+    private final String serviceName;
+
+    /**
+     * Constructs a new TracerFactory.
+     *
+     * @param config Configuration provider
+     * @param openTelemetry OpenTelemetry instance
+     */
+    @Inject
+    public TracerFactory(Config config, OpenTelemetry openTelemetry) {
+        this.openTelemetry = openTelemetry;
+        this.serviceName = config.getString("opentelemetry.service.name", "traccar-reporting");
+    }
+
+    /**
+     * Gets a tracer for the specified instrumentation scope.
+     *
+     * @param instrumentationScope The instrumentation scope name
+     * @return A tracer instance
+     */
+    public Tracer getTracer(String instrumentationScope) {
+        return openTelemetry.getTracer(instrumentationScope, getVersion());
+    }
+
+    /**
+     * Gets the service version.
+     *
+     * @return The service version
+     */
+    private String getVersion() {
+        String version = getClass().getPackage().getImplementationVersion();
+        return version != null ? version : "unknown";
+    }
+}
