899a84a15cbe6a1f7083a4239064b95339dbef15
njdifiore91 <nick@blitzy.com>
2025-05-22 00:08:03 -0400

    File: src/main/java/org/traccar/model/Report.java

--- src/main/java/org/traccar/model/Report.java
+++ src/main/java/org/traccar/model/Report.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 - 2023 Anton Tananaev (anton@traccar.org)
+ * Copyright 2023 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,39 +17,165 @@ package org.traccar.model;
 
 import org.traccar.storage.StorageName;
 
+import java.util.Date;
+
+/**
+ * Represents a generated report in the system.
+ * Stores metadata about reports including their location in object storage.
+ */
 @StorageName("tc_reports")
-public class Report extends ExtendedModel implements Schedulable {
+public class Report extends BaseModel {
 
-    private long calendarId;
-
-    @Override
-    public long getCalendarId() {
-        return calendarId;
-    }
-
-    @Override
-    public void setCalendarId(long calendarId) {
-        this.calendarId = calendarId;
-    }
-
+    private long userId;
+    private long deviceId;
     private String type;
+    private Date createdAt;
+    private Date fromTime;
+    private Date toTime;
+    private String url;
+    private String status;
 
+    /**
+     * Gets the ID of the user who requested the report.
+     *
+     * @return The user ID
+     */
+    public long getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets the ID of the user who requested the report.
+     *
+     * @param userId The user ID
+     */
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Gets the ID of the device for which the report was generated.
+     *
+     * @return The device ID
+     */
+    public long getDeviceId() {
+        return deviceId;
+    }
+
+    /**
+     * Sets the ID of the device for which the report was generated.
+     *
+     * @param deviceId The device ID
+     */
+    public void setDeviceId(long deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    /**
+     * Gets the type of report (e.g., "gpx", "csv", "xlsx").
+     *
+     * @return The report type
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets the type of report.
+     *
+     * @param type The report type
+     */
     public void setType(String type) {
         this.type = type;
     }
 
-    private String description;
-
-    public String getDescription() {
-        return description;
+    /**
+     * Gets the date and time when the report was created.
+     *
+     * @return The creation date
+     */
+    public Date getCreatedAt() {
+        return createdAt;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    /**
+     * Sets the date and time when the report was created.
+     *
+     * @param createdAt The creation date
+     */
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
+    /**
+     * Gets the start date for the report data.
+     *
+     * @return The from date
+     */
+    public Date getFromTime() {
+        return fromTime;
+    }
+
+    /**
+     * Sets the start date for the report data.
+     *
+     * @param fromTime The from date
+     */
+    public void setFromTime(Date fromTime) {
+        this.fromTime = fromTime;
+    }
+
+    /**
+     * Gets the end date for the report data.
+     *
+     * @return The to date
+     */
+    public Date getToTime() {
+        return toTime;
+    }
+
+    /**
+     * Sets the end date for the report data.
+     *
+     * @param toTime The to date
+     */
+    public void setToTime(Date toTime) {
+        this.toTime = toTime;
+    }
+
+    /**
+     * Gets the URL where the report can be accessed.
+     *
+     * @return The report URL
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Sets the URL where the report can be accessed.
+     *
+     * @param url The report URL
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Gets the status of the report (e.g., "pending", "completed", "failed").
+     *
+     * @return The report status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the status of the report.
+     *
+     * @param status The report status
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
