69f8942fd56cb9dde6cb684e704e99c7af5b5ead
njdifiore91 <nick@blitzy.com>
2025-05-21 22:52:58 -0400

    File: src/main/java/org/traccar/session/DeviceSession.java

--- src/main/java/org/traccar/session/DeviceSession.java
+++ src/main/java/org/traccar/session/DeviceSession.java
@@ -15,24 +15,40 @@
  */
 package org.traccar.session;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import org.traccar.BasePipelineFactory;
 import org.traccar.Protocol;
 import org.traccar.model.Command;
 
+import java.io.Serializable;
 import java.net.SocketAddress;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class DeviceSession {
+public class DeviceSession implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private final long deviceId;
     private final String uniqueId;
     private final String model;
     private final Protocol protocol;
-    private final Channel channel;
+    @JsonIgnore
+    private final transient Channel channel;
     private final SocketAddress remoteAddress;
+    
+    // Added for distributed tracing
+    private final String correlationId;
+    
+    // Added for session expiration and versioning
+    private final Instant creationTime;
+    private Instant lastAccessTime;
+    private long version;
 
     public DeviceSession(
             long deviceId, String uniqueId, String model,
@@ -43,6 +59,12 @@ public class DeviceSession {
         this.protocol = protocol;
         this.channel = channel;
         this.remoteAddress = remoteAddress;
+        
+        // Initialize new fields
+        this.correlationId = UUID.randomUUID().toString();
+        this.creationTime = Instant.now();
+        this.lastAccessTime = Instant.now();
+        this.version = 0;
     }
 
     public long getDeviceId() {
@@ -57,6 +79,7 @@ public class DeviceSession {
         return model;
     }
 
+    @JsonIgnore
     public Channel getChannel() {
         return channel;
     }
@@ -70,18 +93,76 @@ public class DeviceSession {
     }
 
     public void sendCommand(Command command) {
+        // Update last access time when sending commands
+        updateLastAccessTime();
         protocol.sendDataCommand(channel, remoteAddress, command);
+    }
+    
+    /**
+     * Get the correlation ID for distributed tracing
+     * 
+     * @return correlation ID as a string
+     */
+    public String getCorrelationId() {
+        return correlationId;
+    }
+    
+    /**
+     * Get the creation time of this session
+     * 
+     * @return creation time as Instant
+     */
+    public Instant getCreationTime() {
+        return creationTime;
+    }
+    
+    /**
+     * Get the last access time of this session
+     * 
+     * @return last access time as Instant
+     */
+    public Instant getLastAccessTime() {
+        return lastAccessTime;
+    }
+    
+    /**
+     * Update the last access time to current time
+     */
+    public void updateLastAccessTime() {
+        this.lastAccessTime = Instant.now();
+    }
+    
+    /**
+     * Get the current version of this session
+     * 
+     * @return version number
+     */
+    public long getVersion() {
+        return version;
+    }
+    
+    /**
+     * Increment the version number
+     * 
+     * @return new version number
+     */
+    public long incrementVersion() {
+        return ++version;
     }
 
     public static final String KEY_TIMEZONE = "timezone";
 
-    private final Map<String, Object> locals = new HashMap<>();
+    // Changed to ConcurrentHashMap for thread safety in distributed environment
+    private final Map<String, Object> locals = new ConcurrentHashMap<>();
 
     public boolean contains(String key) {
+        updateLastAccessTime();
         return locals.containsKey(key);
     }
 
     public void set(String key, Object value) {
+        updateLastAccessTime();
+        incrementVersion();
         if (value != null) {
             locals.put(key, value);
         } else {
@@ -91,7 +172,43 @@ public class DeviceSession {
 
     @SuppressWarnings("unchecked")
     public <T> T get(String key) {
+        updateLastAccessTime();
         return (T) locals.get(key);
     }
-
+    
+    /**
+     * Get a copy of all local attributes
+     * 
+     * @return Map of all local attributes
+     */
+    public Map<String, Object> getLocals() {
+        updateLastAccessTime();
+        return new HashMap<>(locals);
+    }
+    
+    /**
+     * Set multiple local attributes at once
+     * 
+     * @param attributes Map of attributes to set
+     */
+    public void setLocals(Map<String, Object> attributes) {
+        updateLastAccessTime();
+        incrementVersion();
+        if (attributes != null) {
+            locals.putAll(attributes);
+        }
+    }
+    
+    /**
+     * Migrate session data from another session
+     * 
+     * @param other The other session to migrate data from
+     */
+    public void migrateFrom(DeviceSession other) {
+        if (other != null) {
+            updateLastAccessTime();
+            incrementVersion();
+            locals.putAll(other.getLocals());
+        }
+    }
 }
