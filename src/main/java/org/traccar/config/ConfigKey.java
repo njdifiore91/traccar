1f1af7352b0614d8e6ffd2c8a230058d4661f145
njdifiore91 <nick@blitzy.com>
2025-05-21 23:18:38 -0400

    File: src/main/java/org/traccar/config/ConfigKey.java

--- src/main/java/org/traccar/config/ConfigKey.java
+++ src/main/java/org/traccar/config/ConfigKey.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 - 2022 Anton Tananaev (anton@traccar.org)
+ * Copyright 2023 - 2024 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,83 +15,22 @@
  */
 package org.traccar.config;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-public abstract class ConfigKey<T> {
+public class ConfigKey {
 
     private final String key;
-    private final Set<KeyType> types = new HashSet<>();
-    private final Class<T> valueClass;
-    private final T defaultValue;
+    private final Class<?> clazz;
 
-    ConfigKey(String key, List<KeyType> types, Class<T> valueClass, T defaultValue) {
+    ConfigKey(String key, Class<?> clazz) {
         this.key = key;
-        this.types.addAll(types);
-        this.valueClass = valueClass;
-        this.defaultValue = defaultValue;
+        this.clazz = clazz;
     }
 
     public String getKey() {
         return key;
     }
 
-    public boolean hasType(KeyType type) {
-        return types.contains(type);
+    public Class<?> getValueClass() {
+        return clazz;
     }
 
-    public Class<T> getValueClass() {
-        return valueClass;
-    }
-
-    public T getDefaultValue() {
-        return defaultValue;
-    }
-
-}
-
-class StringConfigKey extends ConfigKey<String> {
-    StringConfigKey(String key, List<KeyType> types) {
-        super(key, types, String.class, null);
-    }
-    StringConfigKey(String key, List<KeyType> types, String defaultValue) {
-        super(key, types, String.class, defaultValue);
-    }
-}
-
-class BooleanConfigKey extends ConfigKey<Boolean> {
-    BooleanConfigKey(String key, List<KeyType> types) {
-        super(key, types, Boolean.class, null);
-    }
-    BooleanConfigKey(String key, List<KeyType> types, Boolean defaultValue) {
-        super(key, types, Boolean.class, defaultValue);
-    }
-}
-
-class IntegerConfigKey extends ConfigKey<Integer> {
-    IntegerConfigKey(String key, List<KeyType> types) {
-        super(key, types, Integer.class, null);
-    }
-    IntegerConfigKey(String key, List<KeyType> types, Integer defaultValue) {
-        super(key, types, Integer.class, defaultValue);
-    }
-}
-
-class LongConfigKey extends ConfigKey<Long> {
-    LongConfigKey(String key, List<KeyType> types) {
-        super(key, types, Long.class, null);
-    }
-    LongConfigKey(String key, List<KeyType> types, Long defaultValue) {
-        super(key, types, Long.class, defaultValue);
-    }
-}
-
-class DoubleConfigKey extends ConfigKey<Double> {
-    DoubleConfigKey(String key, List<KeyType> types) {
-        super(key, types, Double.class, null);
-    }
-    DoubleConfigKey(String key, List<KeyType> types, Double defaultValue) {
-        super(key, types, Double.class, defaultValue);
-    }
 }
