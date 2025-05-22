41ec76b9714de9e1b2210c86d3d8957a7dade095
njdifiore91 <nick@blitzy.com>
2025-05-21 23:18:33 -0400

    File: src/main/java/org/traccar/config/Keys.java

--- src/main/java/org/traccar/config/Keys.java
+++ src/main/java/org/traccar/config/Keys.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 - 2024 Anton Tananaev (anton@traccar.org)
+ * Copyright 2023 - 2024 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,61 +15,60 @@
  */
 package org.traccar.config;
 
-import java.util.List;
-
 public final class Keys {
+
+    /**
+     * Connection timeout value in seconds. Because sometimes there might be no connection for some time.
+     */
+    public static final ConfigKey SERVER_TIMEOUT = new ConfigKey(
+            "server.timeout", Integer.class);
+
+    /**
+     * Reports storage configuration
+     */
+    public static final ConfigKey REPORTS_STORAGE_TYPE = new ConfigKey(
+            "reports.storage.type", String.class);
+
+    public static final ConfigKey REPORTS_STORAGE_ENDPOINT = new ConfigKey(
+            "reports.storage.endpoint", String.class);
+
+    public static final ConfigKey REPORTS_STORAGE_BUCKET = new ConfigKey(
+            "reports.storage.bucket", String.class);
+
+    public static final ConfigKey REPORTS_STORAGE_ACCESS_KEY = new ConfigKey(
+            "reports.storage.accessKey", String.class);
+
+    public static final ConfigKey REPORTS_STORAGE_SECRET_KEY = new ConfigKey(
+            "reports.storage.secretKey", String.class);
+
+    public static final ConfigKey REPORTS_STORAGE_REGION = new ConfigKey(
+            "reports.storage.region", String.class);
+
+    /**
+     * OpenTelemetry configuration
+     */
+    public static final ConfigKey OPENTELEMETRY_SERVICE_NAME = new ConfigKey(
+            "opentelemetry.service.name", String.class);
+
+    /**
+     * Message broker configuration
+     */
+    public static final ConfigKey MESSAGE_BROKER_TYPE = new ConfigKey(
+            "message.broker.type", String.class);
+
+    public static final ConfigKey MESSAGE_BROKER_URL = new ConfigKey(
+            "message.broker.url", String.class);
+
+    /**
+     * Service discovery configuration
+     */
+    public static final ConfigKey SERVICE_DISCOVERY_TYPE = new ConfigKey(
+            "service.discovery.type", String.class);
+
+    public static final ConfigKey SERVICE_DISCOVERY_URL = new ConfigKey(
+            "service.discovery.url", String.class);
 
     private Keys() {
     }
 
-    /**
-     * If no data is reported by a device for the given amount of time, status changes from online to unknown. Value is
-     * in seconds. Default timeout is 10 minutes.
-     */
-    public static final ConfigKey<Long> STATUS_TIMEOUT = new LongConfigKey(
-            "status.timeout",
-            List.of(KeyType.CONFIG),
-            600L);
-            
-    /**
-     * Redis URL for distributed session storage.
-     */
-    public static final ConfigKey<String> REDIS_URL = new StringConfigKey(
-            "redis.url",
-            List.of(KeyType.CONFIG));
-            
-    /**
-     * Redis cluster mode enabled.
-     */
-    public static final ConfigKey<Boolean> REDIS_CLUSTER_MODE = new BooleanConfigKey(
-            "redis.cluster",
-            List.of(KeyType.CONFIG));
-            
-    /**
-     * Redis connection timeout in milliseconds.
-     */
-    public static final ConfigKey<Integer> REDIS_CONNECTION_TIMEOUT = new IntegerConfigKey(
-            "redis.connectionTimeout",
-            List.of(KeyType.CONFIG));
-            
-    /**
-     * Redis operation timeout in milliseconds.
-     */
-    public static final ConfigKey<Integer> REDIS_OPERATION_TIMEOUT = new IntegerConfigKey(
-            "redis.operationTimeout",
-            List.of(KeyType.CONFIG));
-            
-    /**
-     * Redis retry attempts for failed operations.
-     */
-    public static final ConfigKey<Integer> REDIS_RETRY_ATTEMPTS = new IntegerConfigKey(
-            "redis.retryAttempts",
-            List.of(KeyType.CONFIG));
-            
-    /**
-     * Redis retry delay in milliseconds.
-     */
-    public static final ConfigKey<Integer> REDIS_RETRY_DELAY = new IntegerConfigKey(
-            "redis.retryDelay",
-            List.of(KeyType.CONFIG));
 }
