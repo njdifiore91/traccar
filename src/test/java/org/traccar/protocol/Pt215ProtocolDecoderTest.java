fccb2c8d8f592c99d41b91a99f3778555a2be890
njdifiore91 <nick@blitzy.com>
2025-05-22 16:15:37 -0400

    File: src/test/java/org/traccar/protocol/Pt215ProtocolDecoderTest.java

--- src/test/java/org/traccar/protocol/Pt215ProtocolDecoderTest.java
+++ src/test/java/org/traccar/protocol/Pt215ProtocolDecoderTest.java
@@ -1,10 +1,21 @@
 package org.traccar.protocol;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.traccar.ProtocolTest;
 
+/**
+ * Test for PT215 protocol decoder.
+ * 
+ * This test class is designed to support both monolithic and microservices testing.
+ * It can be run in the traditional monolithic environment or in the Protocol Service.
+ */
 public class Pt215ProtocolDecoderTest extends ProtocolTest {
 
+    /**
+     * Test basic protocol decoding functionality.
+     * This test works in both monolithic and microservices environments.
+     */
     @Test
     public void testDecode() throws Exception {
 
@@ -18,4 +29,25 @@ public class Pt215ProtocolDecoderTest extends ProtocolTest {
 
     }
 
+    /**
+     * Test protocol integration with message brokers.
+     * This test is only enabled in the microservices environment when the message broker is available.
+     */
+    @Test
+    @EnabledIfSystemProperty(named = "test.broker.enabled", matches = "true")
+    public void testMessageBrokerIntegration() throws Exception {
+        // This test will be implemented when the Protocol Service is fully migrated
+        // It will verify that decoded positions are correctly published to the message broker
+    }
+
+    /**
+     * Test protocol handling across service boundaries.
+     * This test is only enabled in the microservices environment.
+     */
+    @Test
+    @EnabledIfSystemProperty(named = "test.microservices.enabled", matches = "true")
+    public void testCrossServiceIntegration() throws Exception {
+        // This test will be implemented when the Protocol Service is fully migrated
+        // It will verify that the protocol decoder correctly interacts with other services
+    }
 }
