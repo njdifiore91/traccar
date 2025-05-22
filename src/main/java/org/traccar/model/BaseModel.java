9ab2958638b28a4fdbb54306ac1b36670aea50f3
njdifiore91 <nick@blitzy.com>
2025-05-22 04:17:39 -0400

    File: src/main/java/org/traccar/model/BaseModel.java

--- src/main/java/org/traccar/model/BaseModel.java
+++ src/main/java/org/traccar/model/BaseModel.java
@@ -16,16 +16,110 @@
  */
 package org.traccar.model;
 
-public class BaseModel {
+import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * Base model class for all entities in the system.
+ * Provides common fields and functionality for serialization and tracing.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BaseModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * Current schema version for this model.
+     * Should be incremented when the model structure changes.
+     */
+    private int schemaVersion = 1;
+    
+    /**
+     * Tracing context for distributed tracing support.
+     * Used to correlate requests across microservices.
+     */
+    private TracingContext tracingContext;
+    
     private long id;
 
+    /**
+     * Gets the unique identifier of this entity.
+     * 
+     * @return The entity ID
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of this entity.
+     * 
+     * @param id The entity ID to set
+     */
     public void setId(long id) {
         this.id = id;
     }
-
+    
+    /**
+     * Gets the schema version of this model.
+     * 
+     * @return The schema version
+     */
+    public int getSchemaVersion() {
+        return schemaVersion;
+    }
+    
+    /**
+     * Sets the schema version of this model.
+     * 
+     * @param schemaVersion The schema version to set
+     */
+    public void setSchemaVersion(int schemaVersion) {
+        this.schemaVersion = schemaVersion;
+    }
+    
+    /**
+     * Gets the tracing context for distributed tracing.
+     * 
+     * @return The tracing context
+     */
+    public TracingContext getTracingContext() {
+        return tracingContext;
+    }
+    
+    /**
+     * Sets the tracing context for distributed tracing.
+     * 
+     * @param tracingContext The tracing context to set
+     */
+    public void setTracingContext(TracingContext tracingContext) {
+        this.tracingContext = tracingContext;
+    }
+    
+    /**
+     * Validates if the provided schema version is compatible with this model's version.
+     * 
+     * @param version The version to validate against
+     * @return true if the version is compatible, false otherwise
+     */
+    public boolean isVersionCompatible(int version) {
+        return version <= schemaVersion;
+    }
+    
+    /**
+     * Validates the schema version before deserialization.
+     * Throws an exception if the version is incompatible.
+     * 
+     * @param version The version to validate
+     * @throws IllegalArgumentException if the version is incompatible
+     */
+    public void validateVersion(int version) {
+        if (!isVersionCompatible(version)) {
+            throw new IllegalArgumentException(
+                "Incompatible schema version. Expected " + schemaVersion + ", got " + version);
+        }
+    }
 }
