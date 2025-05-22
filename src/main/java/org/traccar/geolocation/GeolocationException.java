93789417b33069b2eefb870f4cd76f770a83fabf
njdifiore91 <nick@blitzy.com>
2025-05-22 05:31:08 -0400

    File: src/main/java/org/traccar/geolocation/GeolocationException.java

--- src/main/java/org/traccar/geolocation/GeolocationException.java
+++ src/main/java/org/traccar/geolocation/GeolocationException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Anton Tananaev (anton@traccar.org)
+ * Copyright 2016 - 2023 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,149 @@
  */
 package org.traccar.geolocation;
 
-public class GeolocationException extends RuntimeException {
+import java.io.Serializable;
 
+/**
+ * Exception thrown during geolocation operations.
+ * Enhanced for microservices environment with correlation ID support for distributed tracing
+ * and service instance information for better error context.
+ */
+public class GeolocationException extends RuntimeException implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String correlationId;
+    private String serviceInstance;
+    private String errorCode;
+    
+    /**
+     * Constructs a new GeolocationException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public GeolocationException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new GeolocationException with the specified detail message and cause.
+     *
+     * @param message the detail message
+     * @param cause the cause of the exception
+     */
+    public GeolocationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    /**
+     * Constructs a new GeolocationException with the specified detail message, correlation ID,
+     * and service instance information.
+     *
+     * @param message the detail message
+     * @param correlationId the correlation ID for distributed tracing
+     * @param serviceInstance the service instance identifier
+     */
+    public GeolocationException(String message, String correlationId, String serviceInstance) {
+        super(message);
+        this.correlationId = correlationId;
+        this.serviceInstance = serviceInstance;
+    }
+    
+    /**
+     * Constructs a new GeolocationException with the specified detail message, cause, correlation ID,
+     * service instance information, and error code.
+     *
+     * @param message the detail message
+     * @param cause the cause of the exception
+     * @param correlationId the correlation ID for distributed tracing
+     * @param serviceInstance the service instance identifier
+     * @param errorCode the error code for categorization
+     */
+    public GeolocationException(String message, Throwable cause, String correlationId, 
+                               String serviceInstance, String errorCode) {
+        super(message, cause);
+        this.correlationId = correlationId;
+        this.serviceInstance = serviceInstance;
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * Gets the correlation ID for distributed tracing.
+     *
+     * @return the correlation ID
+     */
+    public String getCorrelationId() {
+        return correlationId;
+    }
+
+    /**
+     * Sets the correlation ID for distributed tracing.
+     *
+     * @param correlationId the correlation ID to set
+     */
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
+    }
+
+    /**
+     * Gets the service instance identifier.
+     *
+     * @return the service instance identifier
+     */
+    public String getServiceInstance() {
+        return serviceInstance;
+    }
+
+    /**
+     * Sets the service instance identifier.
+     *
+     * @param serviceInstance the service instance identifier to set
+     */
+    public void setServiceInstance(String serviceInstance) {
+        this.serviceInstance = serviceInstance;
+    }
+
+    /**
+     * Gets the error code for categorization.
+     *
+     * @return the error code
+     */
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    /**
+     * Sets the error code for categorization.
+     *
+     * @param errorCode the error code to set
+     */
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+    
+    /**
+     * Returns a string representation of this exception including the message,
+     * correlation ID, service instance, and error code if available.
+     *
+     * @return a string representation of this exception
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(getClass().getName());
+        sb.append(": ").append(getMessage());
+        
+        if (correlationId != null) {
+            sb.append(" [correlationId=").append(correlationId).append("]");
+        }
+        
+        if (serviceInstance != null) {
+            sb.append(" [serviceInstance=").append(serviceInstance).append("]");
+        }
+        
+        if (errorCode != null) {
+            sb.append(" [errorCode=").append(errorCode).append("]");
+        }
+        
+        return sb.toString();
+    }
 }
