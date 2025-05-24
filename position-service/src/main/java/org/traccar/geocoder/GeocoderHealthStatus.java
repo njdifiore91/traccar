fa5ac615fe04a4219f4bce6d38661c764ece6c4b
njdifiore91 <nick@blitzy.com>
2025-05-24 01:55:35 -0400

    File: position-service/src/main/java/org/traccar/geocoder/GeocoderHealthStatus.java

--- position-service/src/main/java/org/traccar/geocoder/GeocoderHealthStatus.java
+++ position-service/src/main/java/org/traccar/geocoder/GeocoderHealthStatus.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 - 2025 Anton Tananaev (anton@traccar.org)
+ * Copyright 2024 Anton Tananaev (anton@traccar.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,185 +17,99 @@ package org.traccar.geocoder;
 
 /**
  * Contains health status information for a geocoder service.
- * This class is used for health reporting and monitoring in a microservices architecture.
+ * This class is used for health reporting and monitoring in the microservices architecture.
  */
 public class GeocoderHealthStatus {
 
-    private final boolean available;
+    private final boolean healthy;
+    private final String statusMessage;
     private final long responseTimeMs;
-    private final int errorCount;
+    private final int successfulRequests;
+    private final int failedRequests;
     private final String lastError;
-    private final String providerName;
-    private final String endpointUrl;
-    private final boolean circuitBreakerOpen;
-    private final long lastSuccessfulRequestTime;
 
     /**
-     * Constructs a new GeocoderHealthStatus with the specified parameters.
+     * Creates a new GeocoderHealthStatus with minimal information.
      *
-     * @param available whether the geocoder service is available
-     * @param responseTimeMs average response time in milliseconds
-     * @param errorCount number of errors encountered
-     * @param lastError description of the last error encountered
-     * @param providerName name of the geocoder provider
-     * @param endpointUrl URL of the geocoder service endpoint
-     * @param circuitBreakerOpen whether the circuit breaker is open
-     * @param lastSuccessfulRequestTime timestamp of the last successful request
+     * @param healthy Whether the geocoder service is healthy
+     * @param statusMessage Status message describing the current state
+     * @param responseTimeMs Average response time in milliseconds
      */
-    public GeocoderHealthStatus(
-            boolean available,
-            long responseTimeMs,
-            int errorCount,
-            String lastError,
-            String providerName,
-            String endpointUrl,
-            boolean circuitBreakerOpen,
-            long lastSuccessfulRequestTime) {
-        this.available = available;
+    public GeocoderHealthStatus(boolean healthy, String statusMessage, long responseTimeMs) {
+        this(healthy, statusMessage, responseTimeMs, 0, 0, null);
+    }
+
+    /**
+     * Creates a new GeocoderHealthStatus with detailed information.
+     *
+     * @param healthy Whether the geocoder service is healthy
+     * @param statusMessage Status message describing the current state
+     * @param responseTimeMs Average response time in milliseconds
+     * @param successfulRequests Count of successful requests
+     * @param failedRequests Count of failed requests
+     * @param lastError Last error message or null if no errors
+     */
+    public GeocoderHealthStatus(boolean healthy, String statusMessage, long responseTimeMs,
+                               int successfulRequests, int failedRequests, String lastError) {
+        this.healthy = healthy;
+        this.statusMessage = statusMessage;
         this.responseTimeMs = responseTimeMs;
-        this.errorCount = errorCount;
+        this.successfulRequests = successfulRequests;
+        this.failedRequests = failedRequests;
         this.lastError = lastError;
-        this.providerName = providerName;
-        this.endpointUrl = endpointUrl;
-        this.circuitBreakerOpen = circuitBreakerOpen;
-        this.lastSuccessfulRequestTime = lastSuccessfulRequestTime;
     }
 
     /**
-     * Returns whether the geocoder service is available.
+     * Gets whether the geocoder service is healthy.
      *
-     * @return true if the service is available, false otherwise
+     * @return true if healthy, false otherwise
      */
-    public boolean isAvailable() {
-        return available;
+    public boolean isHealthy() {
+        return healthy;
     }
 
     /**
-     * Returns the average response time in milliseconds.
+     * Gets the status message describing the current state.
      *
-     * @return average response time in milliseconds
+     * @return Status message
+     */
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    /**
+     * Gets the average response time in milliseconds.
+     *
+     * @return Response time in milliseconds
      */
     public long getResponseTimeMs() {
         return responseTimeMs;
     }
 
     /**
-     * Returns the number of errors encountered.
+     * Gets the count of successful requests.
      *
-     * @return number of errors
+     * @return Count of successful requests
      */
-    public int getErrorCount() {
-        return errorCount;
+    public int getSuccessfulRequests() {
+        return successfulRequests;
     }
 
     /**
-     * Returns the description of the last error encountered.
+     * Gets the count of failed requests.
      *
-     * @return last error description or null if no errors
+     * @return Count of failed requests
+     */
+    public int getFailedRequests() {
+        return failedRequests;
+    }
+
+    /**
+     * Gets the last error message.
+     *
+     * @return Last error message or null if no errors
      */
     public String getLastError() {
         return lastError;
-    }
-
-    /**
-     * Returns the name of the geocoder provider.
-     *
-     * @return provider name
-     */
-    public String getProviderName() {
-        return providerName;
-    }
-
-    /**
-     * Returns the URL of the geocoder service endpoint.
-     *
-     * @return endpoint URL
-     */
-    public String getEndpointUrl() {
-        return endpointUrl;
-    }
-
-    /**
-     * Returns whether the circuit breaker is open.
-     *
-     * @return true if the circuit breaker is open, false otherwise
-     */
-    public boolean isCircuitBreakerOpen() {
-        return circuitBreakerOpen;
-    }
-
-    /**
-     * Returns the timestamp of the last successful request.
-     *
-     * @return timestamp of the last successful request
-     */
-    public long getLastSuccessfulRequestTime() {
-        return lastSuccessfulRequestTime;
-    }
-
-    /**
-     * Builder pattern for creating GeocoderHealthStatus instances.
-     */
-    public static class Builder {
-        private boolean available;
-        private long responseTimeMs;
-        private int errorCount;
-        private String lastError;
-        private String providerName;
-        private String endpointUrl;
-        private boolean circuitBreakerOpen;
-        private long lastSuccessfulRequestTime;
-
-        public Builder available(boolean available) {
-            this.available = available;
-            return this;
-        }
-
-        public Builder responseTimeMs(long responseTimeMs) {
-            this.responseTimeMs = responseTimeMs;
-            return this;
-        }
-
-        public Builder errorCount(int errorCount) {
-            this.errorCount = errorCount;
-            return this;
-        }
-
-        public Builder lastError(String lastError) {
-            this.lastError = lastError;
-            return this;
-        }
-
-        public Builder providerName(String providerName) {
-            this.providerName = providerName;
-            return this;
-        }
-
-        public Builder endpointUrl(String endpointUrl) {
-            this.endpointUrl = endpointUrl;
-            return this;
-        }
-
-        public Builder circuitBreakerOpen(boolean circuitBreakerOpen) {
-            this.circuitBreakerOpen = circuitBreakerOpen;
-            return this;
-        }
-
-        public Builder lastSuccessfulRequestTime(long lastSuccessfulRequestTime) {
-            this.lastSuccessfulRequestTime = lastSuccessfulRequestTime;
-            return this;
-        }
-
-        public GeocoderHealthStatus build() {
-            return new GeocoderHealthStatus(
-                    available,
-                    responseTimeMs,
-                    errorCount,
-                    lastError,
-                    providerName,
-                    endpointUrl,
-                    circuitBreakerOpen,
-                    lastSuccessfulRequestTime);
-        }
     }
 }
