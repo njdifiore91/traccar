c6768ad7ce8844c5e2864e409daa7e395a0627f1
njdifiore91 <nick@blitzy.com>
2025-05-24 02:49:51 -0400

    File: position-service/src/test/java/org/traccar/forward/PositionForwarderUrlTest.java

--- position-service/src/test/java/org/traccar/forward/PositionForwarderUrlTest.java
+++ position-service/src/test/java/org/traccar/forward/PositionForwarderUrlTest.java
@@ -1,60 +1,50 @@
 package org.traccar.forward;
 
 import org.junit.jupiter.api.Test;
+import org.traccar.ProtocolTest;
 import org.traccar.config.Config;
 import org.traccar.config.Keys;
 import org.traccar.model.Device;
 import org.traccar.model.Position;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class PositionForwarderUrlTest {
-
-    protected Position position(String time, boolean valid, double lat, double lon) throws ParseException {
-        Position position = new Position();
-
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        position.setTime(dateFormat.parse(time));
-        position.setValid(valid);
-        position.setLatitude(lat);
-        position.setLongitude(lon);
-
-        return position;
-    }
+/**
+ * Unit test for the PositionForwarderUrl class in the position-service microservice.
+ * This test verifies that the formatRequest method correctly substitutes placeholders
+ * in an HTTP forward URL template.
+ */
+public class PositionForwarderUrlTest extends ProtocolTest {
 
     @Test
     public void testFormatRequest() throws Exception {
-
+        // Create a configuration with a URL template containing placeholders
         Config config = new Config();
         config.setString(Keys.FORWARD_URL, "http://localhost/?fixTime={fixTime}&gprmc={gprmc}&name={name}");
 
+        // Create a position object with test data
         Position position = position("2016-01-01 01:02:03.000", true, 20, 30);
 
+        // Create a mock device with test data
         var device = mock(Device.class);
         when(device.getId()).thenReturn(1L);
         when(device.getName()).thenReturn("test");
         when(device.getUniqueId()).thenReturn("123456789012345");
         when(device.getStatus()).thenReturn(Device.STATUS_ONLINE);
 
+        // Create a position data object with the position and device
         PositionData positionData = new PositionData();
         positionData.setPosition(position);
         positionData.setDevice(device);
 
+        // Create the forwarder with the configuration
         PositionForwarderUrl forwarder = new PositionForwarderUrl(config, null, null);
 
+        // Verify that the formatRequest method correctly substitutes the placeholders
         assertEquals(
                 "http://localhost/?fixTime=1451610123000&gprmc=$GPRMC,010203.000,A,2000.0000,N,03000.0000,E,0.00,0.00,010116,,*05&name=test",
                 forwarder.formatRequest(positionData));
-
     }
-
 }
