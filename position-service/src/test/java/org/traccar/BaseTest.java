462bdce0d47aaf38f7e97a2d139907ac2216d269
njdifiore91 <nick@blitzy.com>
2025-05-24 01:48:44 -0400

    File: position-service/src/test/java/org/traccar/BaseTest.java

---
+++ position-service/src/test/java/org/traccar/BaseTest.java
@@ -0,0 +1,13 @@
+package org.traccar;
+
+/**
+ * Base class for position-service tests.
+ * 
+ * This is a minimal implementation for the GeolocationProviderTest which doesn't
+ * actually use any of the functionality from BaseTest, but extends it for consistency
+ * with the original monolithic structure.
+ */
+public class BaseTest {
+    // This class is intentionally empty as GeolocationProviderTest doesn't use any of its methods
+    // In a real implementation, this would contain utility methods for testing position-service components
+}
